83d3aa42073342a3d31ef5be5499c37eb0a80b20
Jack Rose <user@jackrose.co.nz>
2022-09-02 14:55:03 +1000

    handle changes to existing ignores

--- src/diffIgnore.spec.ts
+++ src/diffIgnore.spec.ts
@@ -1,8 +1,14 @@
-import { diffIgnores } from "./diffIgnores";
+import { diffIgnores, existingMatchesSpec } from "./diffIgnores";
 import { Ignore, IgnoredIssues } from "./types";
 
+/**
+ * setup
+ */
+//
+const ISSUE_ID = "SNYK-JS-NWSAPI-2841516";
+
 const ONE_EXISTING_ISSUE = {
-  "SNYK-JS-NWSAPI-2841516": [
+  [ISSUE_ID]: [
     {
       "*": {
         reason: "",
@@ -15,12 +21,50 @@ const ONE_EXISTING_ISSUE = {
   ],
 };
 
+const ONE_EXISTING_ISSUE_TWO_PATHS = {
+  [ISSUE_ID]: [
+    {
+      "jest > lodash": {
+        reason: "",
+        created: "2022-08-31T07:05:34.723Z",
+        ignoredBy: { id: "x", name: "x", email: "x" },
+        reasonType: "not-vulnerable" as const,
+        disregardIfFixable: false,
+      },
+    },
+    {
+      "webpack > lodash": {
+        reason: "",
+        created: "2022-08-31T07:05:34.723Z",
+        ignoredBy: { id: "x", name: "x", email: "x" },
+        reasonType: "not-vulnerable" as const,
+        disregardIfFixable: false,
+      },
+    },
+  ],
+};
+
 const SPECIFIED_ISSUE_1 = {
   ignorePath: "*",
+  issueId: ISSUE_ID,
+  reasonType: "not-vulnerable" as const,
+};
+
+const SPECIFIED_ISSUE_2 = {
+  ignorePath: "jest > lodash",
+  issueId: ISSUE_ID,
+  reasonType: "not-vulnerable" as const,
+};
+
+const SPECIFIED_ISSUE_3 = {
+  ignorePath: "webpack > lodash",
   issueId: "SNYK-JS-NWSAPI-2841516",
   reasonType: "not-vulnerable" as const,
 };
 
+/**
+ * Testing the main function diffIgnores
+ */
 describe("diffIgnores() can list ignores to update or delete", () => {
   it("returns nothing when passed nothing", () => {
     const specified: Ignore[] = [];
@@ -29,7 +73,6 @@ describe("diffIgnores() can list ignores to update or delete", () => {
     expect(diffIgnores(specified, existing)).toEqual({
       toCreate: [],
       toDelete: [],
-      toUpdate: [],
     });
   });
 
@@ -40,7 +83,6 @@ describe("diffIgnores() can list ignores to update or delete", () => {
     expect(diffIgnores(specified, existing)).toEqual({
       toCreate: [],
       toDelete: [],
-      toUpdate: [],
     });
   });
 
@@ -51,7 +93,74 @@ describe("diffIgnores() can list ignores to update or delete", () => {
     expect(diffIgnores(specified, existing)).toEqual({
       toCreate: [],
       toDelete: ["SNYK-JS-NWSAPI-2841516"],
-      toUpdate: [],
     });
+  });
+
+  it("returns toCreate if not exists", () => {
+    const specified = [SPECIFIED_ISSUE_1];
+    const existing: IgnoredIssues = {};
+
+    expect(diffIgnores(specified, existing)).toEqual({
+      toCreate: [SPECIFIED_ISSUE_1],
+      toDelete: [],
+    });
+  });
+
+  it("when an issue has new path it goes in toDelete and toCreate", () => {
+    const newIgnorePath = "jest > is-even";
+    const specified = [{ ...SPECIFIED_ISSUE_1, ignorePath: newIgnorePath }];
+    const existing = ONE_EXISTING_ISSUE;
+
+    expect(diffIgnores(specified, existing)).toEqual({
+      toCreate: specified,
+      toDelete: [SPECIFIED_ISSUE_1.issueId],
+    });
+  });
+});
+
+/**
+ * Testing the helper function existingMatchesSpec
+ */
+describe("existingMatchesSpec()", () => {
+  it("issueId is specified and exists but different ignorePaths", () => {
+    const specified = [SPECIFIED_ISSUE_2];
+    const existing = ONE_EXISTING_ISSUE_TWO_PATHS[
+      ISSUE_ID
+    ] as unknown as IgnoredIssues[string];
+
+    expect(existingMatchesSpec(ISSUE_ID, existing, specified)).toEqual(false);
+  });
+
+  it("issueId is specified and exists with the same ignorePaths", () => {
+    const specified = [SPECIFIED_ISSUE_2, SPECIFIED_ISSUE_3];
+    const existing = ONE_EXISTING_ISSUE_TWO_PATHS[
+      ISSUE_ID
+    ] as unknown as IgnoredIssues[string];
+
+    expect(existingMatchesSpec(ISSUE_ID, existing, specified)).toEqual(true);
+  });
+
+  it("issueId is specified and exists but reason types given are different", () => {
+    const specified = [
+      { ...SPECIFIED_ISSUE_2, reasonType: "wont-fix" as const },
+      { ...SPECIFIED_ISSUE_3, reasonType: "wont-fix" as const },
+    ];
+    const existing = ONE_EXISTING_ISSUE_TWO_PATHS[
+      ISSUE_ID
+    ] as unknown as IgnoredIssues[string];
+
+    expect(existingMatchesSpec(ISSUE_ID, existing, specified)).toEqual(false);
+  });
+
+  it("issueId is specified and exists but reasons given are different", () => {
+    const specified = [
+      { ...SPECIFIED_ISSUE_2, reason: "new reason" },
+      { ...SPECIFIED_ISSUE_3 },
+    ];
+    const existing = ONE_EXISTING_ISSUE_TWO_PATHS[
+      ISSUE_ID
+    ] as unknown as IgnoredIssues[string];
+
+    expect(existingMatchesSpec(ISSUE_ID, existing, specified)).toEqual(false);
   });
 });
--- src/diffIgnores.ts
+++ src/diffIgnores.ts
@@ -3,14 +3,13 @@ import { Ignore, IgnoredIssues } from "./types";
 type DiffIgnoresResult = {
   toCreate: Ignore[];
   toDelete: string[];
-  toUpdate: never[];
 };
 
 /**
  * `diffIgnores` specified ignores and existing ones and returns lists
  * to act on intentionally left as very imperitive for now while I
  * work out the details
- **/
+ */
 export function diffIgnores(
   specifiedIgnores: Ignore[],
   existingIgnores: IgnoredIssues
@@ -19,11 +18,15 @@ export function diffIgnores(
   const toDelete: string[] = [];
 
   // find specified ignores that don't exist and need to be created
+  // also find ones that need to exist, but are different from what's there
   for (const specifiedIgnore of specifiedIgnores) {
-    const matching = Object.entries(existingIgnores).find(
+    const existingMatch = Object.entries(existingIgnores).find(
       ([key]) => key === specifiedIgnore.issueId
     );
-    if (!matching) {
+    if (!existingMatch) {
+      toCreate.push(specifiedIgnore);
+    } else if (!existingMatchesSpec(...existingMatch, specifiedIgnores)) {
+      toDelete.push(specifiedIgnore.issueId);
       toCreate.push(specifiedIgnore);
     }
   }
@@ -38,6 +41,29 @@ export function diffIgnores(
     }
   }
 
-  // FIXME doesn't account for ignores that need updating
-  return { toCreate, toDelete, toUpdate: [] };
+  return { toCreate, toDelete };
+}
+
+export function existingMatchesSpec(
+  issueId: string,
+  existingIssues: IgnoredIssues[string],
+  specifiedIgnores: Ignore[]
+) {
+  const equalOrFalsy = (x: unknown, y: unknown) => (!x && !y) || x === y;
+
+  for (const [ignorePath, existing] of existingIssues.flatMap(Object.entries)) {
+    const spec = specifiedIgnores.find(
+      (i) => i.issueId === issueId && i.ignorePath === ignorePath
+    );
+
+    if (
+      !spec ||
+      !equalOrFalsy(existing.reason, spec?.reason) ||
+      !equalOrFalsy(existing.reasonType, spec?.reasonType)
+    ) {
+      return false;
+    }
+  }
+
+  return true;
 }
